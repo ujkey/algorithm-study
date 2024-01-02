c3fc2842644747c5a463ea522d743a407a0b1146
ujkey <135115849+ujkey@users.noreply.github.com>
2024-01-02 11:36:01 +0900

    Add: 4_완전탐색 > K번째 큰 수_2차

--- 4_완전탐색/5_K번째 큰 수.js
+++ 4_완전탐색/5_K번째 큰 수.js
@@ -1,6 +1,24 @@
+function solutionB(n, k, card) {
+  let answer;
+  let tmp = new Set();
+
+  // 완전탐색
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let k = j + 1; k < n; k++) {
+        tmp.add(card[i] + card[j] + card[k]);
+      }
+    }
+  }
+
+  const sorted = Array.from(tmp).sort((a, b) => b - a);
+  answer = sorted[k - 1];
+  return answer;
+}
+
 function solutionA(n, k, card) {
   const answer = new Set(); // 3장의 카드를 뽑고 합을 기록
-  const sorted = card.sort((a, b) => b - a);
+  const sorted = card.sort((a, b) => b - a); //DESC
 
   for (let i = 0; i < sorted.length; i++) {
     for (let j = i + 1; j < sorted.length; j++) {
@@ -20,3 +38,4 @@ function solutionA(n, k, card) {
 
 let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
 console.log("solutionA:", solutionA(10, 3, arr));
+console.log("solutionB:", solutionB(10, 3, arr));
