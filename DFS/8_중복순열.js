028d61442f3021efc631b7ce8ab9594b0a156683
ujkey <135115849+ujkey@users.noreply.github.com>
2024-01-26 16:57:08 +0900

    Update: 8_재귀와 완전탐색(DFS) > 중복순열 - 중복을 허락하지 않는 케이스

--- DFS/8_중복순열.js
+++ DFS/8_중복순열.js
@@ -1,5 +1,5 @@
-// 중복을 허락하여 1부터 n까지 자연수 중 m개를 고른 수열
-function solution(n, m) {
+// '중복을 허락하여' 1부터 n까지 자연수 중 m개를 고른 수열
+function solutionA(n, m) {
   let answer = [];
   let tmp = Array.from({ length: m }, () => 0); // 현재까지 선택한 수열을 담을 배열, 초기값 0 설정
 
@@ -21,4 +21,35 @@ function solution(n, m) {
   return answer;
 }
 
-console.log(solution(3, 2));
+console.log("중복 허용: ", solutionA(3, 2));
+
+// '중복을 허락하지 않고' 1부터 n까지 자연수 중 m개를 고른 수열
+function solutionB(n, m) {
+  let answer = [];
+  let tmp = Array.from({ length: m }, () => 0); // 현재까지 선택한 수열을 담을 배열, 초기값 0 설정
+  let visited = Array.from({ length: n }, () => false); // 숫자의 방문 여부를 기록
+
+  function DFS(L) {
+    // L: 현재까지 선택한 숫자의 개수(깊이)
+    if (L === m) {
+      // 최종 레벨에 도달한 경우
+      answer.push(tmp.slice());
+    } else {
+      // 레벨을 증가시키면서 재귀
+      for (let i = 1; i <= n; i++) {
+        if (!visited[i]) {
+          // 방문하지 않은 경우에만
+          visited[i] = true; // 방문 여부를 true로 변경
+          tmp[L] = i;
+          DFS(L + 1); // 다음 레벨로 재귀 호출
+          visited[i] = false; // 재귀가 끝나면 방문 여부를 false로 변경
+        }
+      }
+    }
+  }
+
+  DFS(0);
+  return answer;
+}
+
+console.log("중복 미허용: ", solutionB(3, 2));
