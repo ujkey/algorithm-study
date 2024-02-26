fdfd0a398c0cb26be5446fc7451dda93d4a630ea
ujkey <135115849+ujkey@users.noreply.github.com>
2024-02-26 09:34:35 +0900

    Update: 구현 > 백준 > 14501_퇴사

--- 구현/14501.js
+++ 구현/14501.js
@@ -9,30 +9,24 @@ const resource = require("fs")
   .split("\n");
 
 const N = resource.shift() * 1; // 퇴사까지 남은 날짜
-const table = resource.map((el) => el.split(" ").map((el) => el * 1)); // 상담을 완료하는데 걸리는 기간과 수익
+const table = resource.map((el) => el.split(" ").map(Number)); // 상담을 완료하는데 걸리는 기간과 수익
 
 function solution(N, table) {
-  let answer = 0; // 최대 수익
-  let time = N; // 남은 날짜
+  const DP = new Array(N).fill(0); // DP[i]: i일까지의 최대 수익
+  // console.log(DP);
 
-  // 종료조건 :
-  while (time > 0) {
-    const cur = N - time; // 남은 날짜
-    const [t, p] = table[cur]; // t: 상담 소요 기간, p: 수익
-    console.log(cur, t, p);
-    if (time - t < 0) break;
+  for (let i = 0; i < N; i++) {
+    const [period, profit] = table[i];
 
-    // 상담 소요 기간만큼 날짜를 줄인다
-    time -= t;
+    if (i + period > N) continue;
+    DP[i] = DP[i] + profit;
 
-    // 수익 계산
-    answer += p;
-    console.log(answer);
+    for (let j = i + period; j < N; j++) {
+      DP[j] = Math.max(DP[j], DP[i]);
+    }
   }
 
-  //   console.log(answer);
+  console.log(Math.max(...DP));
 }
 
 solution(N, table);
-
-// 예제 4번 통과 못하는 이유는?
